35006e49bdb926a7cdcbe4f5aa14e959921c800c
Thirumalesh-63 <gtm5562@gmail.com>
2024-11-28 15:31:53 +0530

    Revert "springmvc7"
    This reverts commit daf206258b771095c1c081dd80d71fff040f84a8.

--- src/main/java/Firstmvc/Mvcex/person/bean/student.java
+++ src/main/java/Firstmvc/Mvcex/person/bean/student.java
@@ -13,7 +13,7 @@ public class student {
     private double salary;
     private Long phno;
 
-    private String village;
+    private String city;
 
     public int getId() {
         return id;
@@ -64,21 +64,21 @@ public class student {
     }
 
     public String getCity() {
-        return village;
+        return city;
     }
 
-    public void setCity(String village) {
-        this.village = village;
+    public void setCity(String city) {
+        this.city = city;
     }
 
-    public student(int id, String name, String age, String dept, double salary, Long phno,String village) {
+    public student(int id, String name, String age, String dept, double salary, Long phno,String city) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.dept = dept;
         this.salary = salary;
         this.phno = phno;
-        this.village=village;
+        this.city=city;
     }
 
     public student() {
