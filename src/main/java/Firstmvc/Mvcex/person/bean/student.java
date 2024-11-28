b429a1f238a4773a19cc97ea424fd112c1b1fa02
Thirumalesh-63 <gtm5562@gmail.com>
2024-11-28 15:03:01 +0530

    springmvc6

--- src/main/java/Firstmvc/Mvcex/person/bean/student.java
+++ src/main/java/Firstmvc/Mvcex/person/bean/student.java
@@ -12,17 +12,8 @@ public class student {
 
     private double salary;
     private Long phno;
-    public student() {
-    }
 
-    public student(int id, String name,String age,long phno,String dept,Double salary) {
-        this.id = id;
-        this.name = name;
-        this.age=age;
-        this.phno=phno;
-        this.dept=dept;
-        this.salary=salary;
-    }
+    private String city;
 
     public int getId() {
         return id;
@@ -30,14 +21,6 @@ public class student {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
     }
 
     public String getName() {
@@ -48,14 +31,6 @@ public class student {
         this.name = name;
     }
 
-     public String getDept() {
-        return dept;
-    }
-
-    public void setDept(String dept) {
-        this.dept = dept;
-    }
-
     public String getAge() {
         return age;
     }
@@ -64,11 +39,48 @@ public class student {
         this.age = age;
     }
 
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
     public Long getPhno() {
         return phno;
     }
 
     public void setPhno(Long phno) {
         this.phno = phno;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public student(int id, String name, String age, String dept, double salary, Long phno,String city) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.dept = dept;
+        this.salary = salary;
+        this.phno = phno;
+        this.city=city;
+    }
+
+    public student() {
     }
 }
