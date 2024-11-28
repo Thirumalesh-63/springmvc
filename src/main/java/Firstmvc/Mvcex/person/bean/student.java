c6ad0ebe8c94272375b16f4aa2eddaba7882442f
Thirumalesh-63 <129723705+Thirumalesh-63@users.noreply.github.com>
2024-11-28 15:54:36 +0530

    Update student.java

--- src/main/java/Firstmvc/Mvcex/person/bean/student.java
+++ src/main/java/Firstmvc/Mvcex/person/bean/student.java
@@ -4,7 +4,7 @@ public class student {
 
     private int id;
 
-    private String name;
+    private String fname;
 
     private String age;
 
@@ -24,11 +24,11 @@ public class student {
     }
 
     public String getName() {
-        return name;
+        return fname;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String fname) {
+        this.fname = fname;
     }
 
     public String getAge() {
@@ -71,9 +71,9 @@ public class student {
         this.city = city;
     }
 
-    public student(int id, String name, String age, String dept, double salary, Long phno,String city) {
+    public student(int id, String fname, String age, String dept, double salary, Long phno,String city) {
         this.id = id;
-        this.name = name;
+        this.fname = fname;
         this.age = age;
         this.dept = dept;
         this.salary = salary;
