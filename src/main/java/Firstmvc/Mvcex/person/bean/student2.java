a14c2d55f5b5de1a48dd987103ebc9335015bf27
Thirumalesh-63 <129723705+Thirumalesh-63@users.noreply.github.com>
2024-12-04 16:33:23 +0530

    Update student2.java

--- src/main/java/Firstmvc/Mvcex/person/bean/student2.java
+++ src/main/java/Firstmvc/Mvcex/person/bean/student2.java
@@ -1,12 +1,12 @@
 package Firstmvc.Mvcex.person.bean;
 
 public class student2 {
-    private int id3;
+    private int id;
 
-    private String firstname3;
+    private String firstname;
 
-    private String gender3;
+    private String gender;
 
-    private String deptartment3;
+    private String deptartment;
 
 }
