7bf43bf6776b90b7b1074c42d12ebab954d4bd9b
Thirumalesh-63 <gtm5562@gmail.com>
2024-12-04 18:03:45 +0530

    springmvc-8

--- src/main/java/Firstmvc/Mvcex/person/bean/student2.java
+++ src/main/java/Firstmvc/Mvcex/person/bean/student2.java
@@ -1,11 +1,11 @@
 package Firstmvc.Mvcex.person.bean;
 
 public class student2 {
-    private int id2;
+    private int id;
 
-    private String firstname1;
+    private String firstname;
 
-    private String gender1;
+    private String gender;
 
     private String deptartment;
 
