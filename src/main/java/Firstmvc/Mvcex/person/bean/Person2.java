b0ce437d6ef51184747a41ce0848ea130a182120
Thirumalesh-63 <129723705+Thirumalesh-63@users.noreply.github.com>
2024-12-20 11:42:20 +0530

    Update Person2.java

--- src/main/java/Firstmvc/Mvcex/person/bean/Person2.java
+++ src/main/java/Firstmvc/Mvcex/person/bean/Person2.java
@@ -4,11 +4,30 @@ public class Person2 {
 
     int id;
 
+    String name;
+
+    int age;
+
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+     public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.Name = name;
     }
 }
