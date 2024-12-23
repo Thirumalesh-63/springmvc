71d85b4a9eb7052e7aed88d706fb283da041f0df
Thirumalesh-63 <129723705+Thirumalesh-63@users.noreply.github.com>
2024-12-23 10:36:17 +0530

    Update Person.java

--- src/main/java/Firstmvc/Mvcex/person/bean/Person.java
+++ src/main/java/Firstmvc/Mvcex/person/bean/Person.java
@@ -3,12 +3,12 @@ package Firstmvc.Mvcex.person.bean;
 public class Person {
 	@Override
 	public String toString() {
-		return "Person [id=" + id3 + ", name=" + name + ", sal=" + sal + ", company=" + company + "]";
+		return "Person [id=" + id3 + ", name=" + name3 + ", sal=" + sal3 + ", company=" + company3 + "]";
 	}
-	int id
-	String name;
-	int sal;
-	String company;
+	int id3;
+	String name3;
+	int sal3;
+	String company3;
 	public int getId() {
 		return id3;
 	}
@@ -16,22 +16,22 @@ public class Person {
 		this.id3 = id;
 	}
 	public String getName() {
-		return name;
+		return name3;
 	}
 	public void setName(String name) {
-		this.name = name;
+		this.name3 = name;
 	}
 	public int getSal() {
-		return sal;
+		return sal3;
 	}
 	public void setSal(int sal) {
-		this.sal = sal;
+		this.sal3 = sal;
 	}
 	public String getCompany() {
-		return company;
+		return company3;
 	}
 	public void setCompany(String company) {
-		this.company = company;
+		this.company3 = company;
 	}
 	public Person() {
 		super();
@@ -40,9 +40,9 @@ public class Person {
 	public Person(int id, String name, int sal, String company) {
 		super();
 		this.id3 = id;
-		this.name = name;
-		this.sal = sal;
-		this.company = company;
+		this.name3 = name;
+		this.sal3 = sal;
+		this.company3 = company;
 	}
 	
 
