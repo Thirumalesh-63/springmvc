3b34ecaeb818ac273f92152d4d6c9f6341e3568a
Thirumalesh-63 <129723705+Thirumalesh-63@users.noreply.github.com>
2024-12-23 15:22:12 +0530

    springmvc_4 (#34)
    * springmvc_3
    
    * springmvc_4

--- src/main/java/Firstmvc/Mvcex/person/bean/Person.java
+++ src/main/java/Firstmvc/Mvcex/person/bean/Person.java
@@ -3,35 +3,35 @@ package Firstmvc.Mvcex.person.bean;
 public class Person {
 	@Override
 	public String toString() {
-		return "Person [id=" + id4 + ", name=" + name4 + ", sal=" + sal4 + ", company=" + company4 + "]";
+		return "Person [id=" + id3 + ", name=" + name3 + ", sal=" + sal3 + ", company=" + company3 + "]";
 	}
-	int id4;
-	String name4;
-	int sal4;
-	String company4;
+	int id3;
+	String name3;
+	int sal3;
+	String company3;
 	public int getId() {
-		return id4;
+		return id3;
 	}
 	public void setId(int id) {
-		this.id4 = id;
+		this.id3 = id;
 	}
 	public String getName() {
-		return name4;
+		return name3;
 	}
 	public void setName(String name) {
-		this.name4 = name;
+		this.name3 = name;
 	}
 	public int getSal() {
-		return sal4;
+		return sal3;
 	}
 	public void setSal(int sal) {
-		this.sal4 = sal;
+		this.sal3 = sal;
 	}
 	public String getCompany() {
-		return company4;
+		return company3;
 	}
 	public void setCompany(String company) {
-		this.company4 = company;
+		this.company3 = company;
 	}
 	public Person() {
 		super();
@@ -40,9 +40,9 @@ public class Person {
 	public Person(int id, String name, int sal, String company) {
 		super();
 		this.id4 = id;
-		this.name4 = name;
-		this.sal4 = sal;
-		this.company4 = company;
+		this.name3 = name;
+		this.sal3 = sal;
+		this.company3 = company;
 	}
 	
 
