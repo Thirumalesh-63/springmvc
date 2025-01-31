ef210c43fc88f521bab144a0168fa5ef742e83fa
Thirumalesh-63 <129723705+Thirumalesh-63@users.noreply.github.com>
2025-01-31 10:43:35 +0530

    Update Person.java

--- src/main/java/Firstmvc/Mvcex/person/bean/Person.java
+++ src/main/java/Firstmvc/Mvcex/person/bean/Person.java
@@ -5,33 +5,33 @@ public class Person {
 	public String toString() {
 		return "Person [id=" + id3 + ", name=" + name3 + ", sal=" + sal3 + ", company=" + company3 + "]";
 	}
-	int id3;
-	String name3;
-	int sal3;
-	String company3;
+	int id;
+	String name;
+	int sal;
+	String company;
 	public int getId() {
-		return id3;
+		return id;
 	}
 	public void setId(int id) {
-		this.id3 = id;
+		this.id = id;
 	}
 	public String getName() {
 		return name3;
 	}
 	public void setName(String name) {
-		this.name3 = name;
+		this.name = name;
 	}
 	public int getSal() {
-		return sal3;
+		return sal;
 	}
 	public void setSal(int sal) {
-		this.sal3 = sal;
+		this.sal = sal;
 	}
 	public String getCompany() {
-		return company3;
+		return company;
 	}
 	public void setCompany(String company) {
-		this.company3 = company;
+		this.company = company;
 	}
 	public Person() {
 		super();
@@ -39,10 +39,10 @@ public class Person {
 	}
 	public Person(int id, String name, int sal, String company) {
 		super();
-		this.id3 = id;
-		this.name3 = name;
-		this.sal3 = sal;
-		this.company3 = company;
+		this.id = id;
+		this.name = name;
+		this.sal = sal;
+		this.company = company;
 	}
 	
 
