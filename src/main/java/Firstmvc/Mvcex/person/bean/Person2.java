33fe9fb967cbfd27fe8ef2efe0db0afb77d1dfc7
Thirumalesh-63 <gtm5562@gmail.com>
2025-02-03 15:15:56 +0530

    messages

--- src/main/java/Firstmvc/Mvcex/person/bean/Person2.java
+++ src/main/java/Firstmvc/Mvcex/person/bean/Person2.java
@@ -2,43 +2,43 @@ package Firstmvc.Mvcex.person.bean;
 
 public class Person2 {
 
-    int id;
+    int id2;
 
-    String name;
+    String name2;
 
-    int age;
+    int age2;
 
-    int salary;
+    int salary2;
 
     public String getName() {
-        return name;
+        return name2;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name2 = name;
     }
 
     public int getAge() {
-        return age;
+        return age2;
     }
 
     public void setAge(int age) {
-        this.age = age;
+        this.age2 = age;
     }
 
     public int getSalary() {
-        return salary;
+        return salary2;
     }
 
     public void setSalary(int salary) {
-        this.salary = salary;
+        this.salary2 = salary;
     }
 
     public int getId() {
-        return id;
+        return id2;
     }
 
     public void setId(int id) {
-        this.id = id;
+        this.id2 = id;
     }
 }
