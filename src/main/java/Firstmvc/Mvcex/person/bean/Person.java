f3a2b55f46370a89ff500fd65aaa2e746688cd3c
Thirumalesh-63 <gtm5562@gmail.com>
2025-02-03 14:45:36 +0530

    message

--- src/main/java/Firstmvc/Mvcex/person/bean/Person.java
+++ src/main/java/Firstmvc/Mvcex/person/bean/Person.java
@@ -1,50 +1,50 @@
 package Firstmvc.Mvcex.person.bean;
 
 public class Person {
-    int id2;
-    String name2;
-    int sal2;
-    String company2;
+    int id3;
+    String name3;
+    int sal3;
+    String company3;
 
     public int getId() {
-        return id2;
+        return id3;
     }
 
     public void setId(int id) {
-        this.id2 = id;
+        this.id3 = id;
     }
 
     public String getName() {
-        return name2;
+        return name3;
     }
 
     public void setName(String name) {
-        this.name2 = name;
+        this.name3 = name;
     }
 
     public int getSal() {
-        return sal2;
+        return sal3;
     }
 
     public void setSal(int sal) {
-        this.sal2 = sal;
+        this.sal3 = sal;
     }
 
     public String getCompany() {
-        return company2;
+        return company3;
     }
 
     public void setCompany(String company) {
-        this.company2 = company;
+        this.company3 = company;
     }
 
     public Person() {
     }
 
     public Person( int id,String name, int sal, String company) {
-        this.name2 = name;
-        this.id2 = id;
-        this.sal2 = sal;
-        this.company2 = company;
+        this.name3 = name;
+        this.id3 = id;
+        this.sal3 = sal;
+        this.company3 = company;
     }
 }
